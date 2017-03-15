9ec3eb396c19ab5d9ac3e93059190c5888d66358
rajeevbrahma <rajeevbrahma@hotmail.com>
2017-03-15 11:32:58 +0530

    Block

---
+++ Block/main.js
@@ -0,0 +1,186 @@
+export default (request) => {
+    // Required modules
+    const db = require("kvstore"); // Database module
+    const pubnub = require("pubnub"); // xmlHttp request module
+    const xhr = require("xhr");
+    const Promise = require('promise');
+    
+    var date_const = new Date(); // Date constructor
+	var message_dict = {};
+	
+    const basicAuth = require('codec/auth');
+    var username = '9dbcd599-cd60-4196-8a43-8e2ef7f5af1d'; // Username for the conversation api
+    var password = 'U4ptOB82jfp4';                         // password for the conversation api
+    var auth = basicAuth.basic(username,password);
+    
+    var TimeLimit = 2;
+    var minute = 60*60*24; // In minutes
+    var diff_inminutes = null;
+
+
+    console.log(auth);
+    const url = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&tones=emotion";
+
+    // http options for the rest call.
+    const http_options = {
+        "method": "POST",
+        
+        "headers": {
+                "Content-Type": "application/json",
+                "Authorization":auth
+    },
+            "body":{}
+    };
+    
+    var AngerAvgScore = 0;
+    var FearAvgScore = 0;
+    var SadnessAvgScore = 0;
+    var DisgustAvgScore = 0;
+    var JoyAvgScore = 0;
+    var xhrfetchcalllist = [];   
+    var tweetlength = request.message.twitterfeed.length;
+    var pubchannel = "realtimebrandmonitor_resp";
+    var twitterfeed = request.message.twitterfeed;
+    var brandname =  request.message.brandname; 
+    
+            
+   
+    
+    var tonescoreDict = {"AngerAvgScore":0,"FearAvgScore":0,"SadnessAvgScore":0,"DisgustAvgScore":0,"JoyAvgScore":0}
+        
+    
+    function sendToApp(brandname)
+    {
+        console.log("SENDTOAPP",brandname);
+        return db.get(brandname).then((database_value) => {
+            console.log(" DATA IN DATABASE -- >",database_value);
+            var database_tweetscore = database_value.tweetscore;
+            var tweetscore = {};
+            for (var key in database_tweetscore){
+                tweetscore[key] = database_tweetscore[key]/tweetlength;
+            }
+            console.log("AFTER AVERAGE CALCULATION",tweetscore)
+            request.message.tonescore = tweetscore;
+            delete request.message.twitterfeed;
+            console.log(request.message,"INSIDE SENDTOAPP");
+            return request;
+            });
+    }
+
+
+    function api_call(){
+        for (var i=0;i<twitterfeed.length;i++) 
+            {
+            http_options.body = {"text":twitterfeed[i][Object.keys(twitterfeed[i])[0]]};
+            xhrfetchcalllist.push(xhr.fetch(url,http_options));
+            }
+
+   
+            return Promise.all(xhrfetchcalllist)
+                .then(function (responses) {
+                    for (var j=0;j<responses.length;j++)
+                    {
+                     const body = JSON.parse(responses[j].body);
+                        
+                        
+                        for (var t=0;t<body.document_tone.tone_categories[0].tones.length;t++)
+                        {
+                            var scoredetails = body.document_tone.tone_categories[0].tones[t]
+                            // console.log(scoredetails);
+                            if (scoredetails.tone_id == "anger")
+                            {
+                                AngerAvgScore = AngerAvgScore+scoredetails.score;
+                        
+
+                            }
+                            if (scoredetails.tone_id == "fear")
+                            {
+                                FearAvgScore = FearAvgScore+scoredetails.score;
+                        
+                            }
+                            if (scoredetails.tone_id == "disgust")
+                            {
+                                
+                                DisgustAvgScore = DisgustAvgScore+scoredetails.score;
+                        
+                            }
+                            if (scoredetails.tone_id == "sadness")
+                            {
+                                SadnessAvgScore = SadnessAvgScore+scoredetails.score;
+                            }
+                            if (scoredetails.tone_id == "joy")
+                            {
+                                JoyAvgScore = JoyAvgScore+scoredetails.score;
+                        
+                            }
+
+                            
+                        }
+                        tonescoreDict = {"tweetscore":{"AngerAvgScore":AngerAvgScore,"FearAvgScore":FearAvgScore,"SadnessAvgScore":SadnessAvgScore,"DisgustAvgScore":DisgustAvgScore,"JoyAvgScore":JoyAvgScore},"lastupdatedTime":date_const.getTime()};
+                        console.log("INDIVIDUAL",tonescoreDict);
+                         
+                    }
+                    console.log("FINAL",tonescoreDict);
+
+                    db.set(brandname,tonescoreDict);
+                    var tweetscore_dict = {};
+                    var tweetscore_apicall = {};
+                    
+                    tweetscore_dict = tonescoreDict.tweetscore;
+
+                    console.log(tweetscore_dict,tweetlength,"TWEET SCORE, TWEET LENGTH");
+                    for (var tweetkey in tweetscore_dict){
+                        tweetscore_apicall[tweetkey] = tweetscore_dict[tweetkey]/tweetlength;
+                    }
+                    console.log("AFTER AVERAGE CALCULATION",tweetscore_apicall);
+                    delete request.message.twitterfeed;
+                    request.message.tonescore = tweetscore_apicall;
+                    console.log(request.message,"REQUEST MESSAGE API CALL");
+                
+                    return request;
+                    
+                });
+                    
+            // return request;    
+           
+    }
+
+
+
+
+    return db.get(brandname).then((database_value) =>{
+    
+        if (database_value)
+        {
+            var currentTime = date_const.getTime(); // Getting the Current time
+            var lastupdatedTime = database_value.lastupdatedTime; // Extracting the Lastupdate time from the Database.
+            
+            var diff_inminutes = Math.round((currentTime - lastupdatedTime)/minute); // Calculating Time difference
+            
+            console.log("TIME DIFFERENCE IN MINUTES --> ",diff_inminutes);
+            
+             if (diff_inminutes>=TimeLimit)
+            {
+                return api_call().then((x)=>{
+                    return request;
+                });                
+                console.log("15 MIN ONCE");
+            }
+            else{
+                
+                return sendToApp(brandname).then((x)=>{
+                    return request;
+                });
+                
+            }
+        }
+        else{
+            return api_call().then((x)=>{
+                    return request;
+                }); 
+            console.log("FOR THE FIRST TIME");                 
+        } 
+        return request.ok();  
+    });
+   // return request.ok(); 
+};
